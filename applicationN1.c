969393b2a6a7269abcae56a8304316a5fdb396c7
0penVar <71385626+0penVar@users.noreply.github.com>
2021-05-02 21:48:08 -0400

    Update applicationN1.c

--- applicationN1.c
+++ applicationN1.c
@@ -8,9 +8,10 @@ int main(int argc, char *argv[])
 
     do
     {
+        printf("-----------------------------------------------------------------------\n");
+        printf("\n\t\tQuestion 1\nCombien y-a-t'il de planete dans notre systeme solaire\n\n");
+        printf("-----------------------------------------------------------------------\n");
 
-        printf("\n\tQuestion N1\ncombien y-a-t'il de planete dans notre systeme solaire\n");
-        printf("-----------------------------------------------------------------------");
         do
         {
             printf("\nreponse 1 : c'est 1");
@@ -23,22 +24,27 @@ int main(int argc, char *argv[])
             printf("\nreponse 8 : c'est 8");
             printf("\nreponse 9 : c'est 9");
             printf("\nreponse 10 : c'est 10");
-            printf("\nreponse 11 : c'est 11");
+            printf("\n\nVeuillez marquer votre reponse ici : ");
             scanf("%d", &a);
 
             if(a != 8)
             {
+                printf("\n\n-----------------------------------------------------------------------\n");
                 printf("\nMauvaise reponse\n");
                 score -= 1;
+                printf("\n-----------------------------------------------------------------------\n");
             }
             else if(a = 8)
             {
+                printf("\n\n-----------------------------------------------------------------------\n");
                 printf("\nBonne reponse");
-                printf("\nVotre score est de %d dur 10", score);
+                printf("\nVotre score est de %d sur 10", score);
+                printf("\n\n-----------------------------------------------------------------------\n");
             }
         }while(a != 8);
 
         printf("\n\nPour refaire une partie, appuyez sur 1, sinon appuyez sur 0");
+        printf("\nVeuillez marquer votre choix ici : ");
         scanf("%d", &refaire);
 
     }while(refaire);
