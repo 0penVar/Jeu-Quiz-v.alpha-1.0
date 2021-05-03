7dfc5dddcccb23778b16481f8d9c6e0c9fffcd0f
0penVar <71385626+0penVar@users.noreply.github.com>
2021-05-02 21:54:22 -0400

    Create Quiz1.h

---
+++ applicationN1/Quiz1.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+
+
+#endif // MAIN_H_INCLUDED
+
+// prototypes void ???(int, int)
+void alpha(int, int);
+
+void beta(int, int);
+
+void antares(int, int);
+
+
+// prototypes void ???(double, double)
+void delta(double, double);
+
+void teta(double, double);
+
+void omega(double, double);
+
+//Pour l'instant, il n'y a aucune utilité à ce fichier
